1e71c53772cd637a08fea380b7d8a1d06267aeab
Marcos Romero <-e>
2023-11-25 01:22:23 -0500

    Pagina de Validacion de emails, terminada

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded',() => {
-    const formulario = document.querySelector('#contenedor-input')
+    const formulario = document.querySelector('#contenedor')
     const btnComprobar = document.querySelector('#btnComprobar')
+    const inputCorreo = document.querySelector('#correo')
     btnComprobar.addEventListener('click', presionarBoton)
 
 
@@ -12,23 +13,39 @@ document.addEventListener('DOMContentLoaded',() => {
     }
 
     function validarCorreo(valor){
-        if(!valor.trim()) {
-            alertaVacio()
-        }
+        (!valor.trim())?  mostrarAlerta('No puede dejar vacio el campo',false) : comprobarEmail();
+                   
     }
 
-    function alertaVacio(){
+    function mostrarAlerta(mensaje, enviado){
         // Seleccion de input
-        const input = document.querySelector('')
-
+        limpiarAlertas()
         const alerta = document.createElement('P')
-        alerta.textContent = 'No puede dejar vacio el campo'
+        enviado?  inputCorreo.classList.add('border-green-500') : inputCorreo.classList.add('border-red-500')
+        alerta.className = 'error px-4 text-xs'
+        enviado? alerta.classList.add('text-green-500') : alerta.classList.add('text-red-500');
 
-
-        alerta.className = 'block'
-
-
-
+        enviado? setTimeout(() => {
+            limpiarAlertas()
+            inputCorreo.classList.remove('border-green-500')
+            inputCorreo.classList.remove('border-red-500')
+            inputCorreo.value = ''
+        }, 3000) : null;
+        
+        alerta.textContent = mensaje
         formulario.appendChild(alerta)
+    }
+
+
+    function comprobarEmail(){
+        let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        (regex.test(inputCorreo.value))? mostrarAlerta('Gracias por suscribirte',true) : mostrarAlerta('El correo es invalido',false) 
+    }
+
+
+    function limpiarAlertas(){
+        if(formulario.children[1]){
+            formulario.removeChild(formulario.children[1])
+        }
     }
 } )
